3e456d45bea67ab92c5d114ca1ec27e7461b751b
PhyoLinMg <gangsterplm@gmail.com>
2025-04-14 17:12:42 +0700

    netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,61 +1,40 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import compression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ReactPortfolio/',
-  // plugins: [
-  //   // react({
-  //   //   babel: {
-  //   //     plugins: ['@emotion/babel-plugin']
-  //   //   }
-  //   // }),
-  //   // compression({
-  //   //   algorithm: 'brotliCompress',
-  //   //   ext: '.br',
-  //   //   threshold: 512,
-  //   //   compressionOptions: { level: 11 }
-  //   // }),
-  //   // compression({
-  //   //   algorithm: 'gzip',
-  //   //   ext: '.gz',
-  //   //   threshold: 512,
-  //   //   compressionOptions: { level: 9 }
-  //   // }),
-  //   // visualizer({
-  //   //   open: true,
-  //   //   gzipSize: true,
-  //   //   brotliSize: true,
-  //   // })
-  // ],
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+    compression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+  ],
   build: {
+    outDir: 'dist',
+    sourcemap: true,
     rollupOptions: {
-      // output: {
-      //   manualChunks: {
-      //     vendor: ['react', 'react-dom', 'framer-motion', '@emotion/react', '@emotion/styled'],
-      //     icons: ['react-icons'],
-      //   },
-      // },
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'framer-motion': ['framer-motion'],
+          'react-icons': ['react-icons'],
+        },
+      },
     },
-    chunkSizeWarningLimit: 1000,
-    // minify: 'terser',
-    // terserOptions: {
-    //   compress: {
-    //     drop_console: true,
-    //     drop_debugger: true,
-    //     pure_funcs: ['console.log'],
-    //     passes: 2,
-    //     ecma: 2020
-    //   },
-    //   mangle: {
-    //     safari10: true
-    //   },
-    //   format: {
-    //     comments: false
-    //   }
-    // },
-    // reportCompressedSize: true,
   },
-  // optimizeDeps: {
-  //   include: ['react', 'react-dom', 'framer-motion', '@emotion/react', '@emotion/styled', 'react-icons'],
-  // },
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
+  base: '/',
 })

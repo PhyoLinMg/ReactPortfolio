d7ba52c862538f8eb89f13c66dd913e714da330a
PhyoLinMg <gangsterplm@gmail.com>
2025-05-05 23:05:04 +0700

    remove console test

--- src/network/client/getDatoCmsToken.ts
+++ src/network/client/getDatoCmsToken.ts
@@ -1,5 +1,4 @@
 // getDatoCmsToken.ts
 export const getDatoCmsToken = (): string => {
-    console.log("cms token", import.meta.env.VITE_CMS_TOKEN);
     return import.meta.env.VITE_CMS_TOKEN ?? "";
 };  
